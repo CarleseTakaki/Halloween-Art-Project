6788d871f53b64c75ec0e5a56635e6c48eeb1419
CarleseTakaki <91640229+CarleseTakaki@users.noreply.github.com>
2021-11-01 18:49:28 -0700

    Update Halloween_Art_Project.py

--- Halloween_Art/Halloween_Art_Project.py
+++ Halloween_Art/Halloween_Art_Project.py
@@ -1,41 +1,32 @@
 import turtle as trtl
-tr = trtl.Turtle()
 wn = trtl.Screen()
-wn.addshape('halloween-pusheen.gif')
-tr.shape('halloween-pusheen.gif')
-wn.mainloop()
+
+wn.addshape('bugbug2.gif')
+
+
+wn.bgpic("halloween-pusheen.gif.gif")
+
 import random as rand
 
-#-----game configuration----
-bubble_color = "green"
-bubble_size = int(2)
-bubble_shape = "circle"
-a=0
-b=400
-c=0
-d=300
-
-#-----initialize turtle-----
 bubble = trtl.Turtle()
+bubble.shape('bugbug2.gif')
 
-#-----game functions--------
-bubble.fillcolor(bubble_color)
-bubble.shapesize(bubble_size)
-bubble.shape(bubble_shape)
-
-def change_position ():
-  new_xpos = rand.randint(a,b)
-  new_ypos = rand.randint(c,d)
+def change_position():
+  new_xpos = rand.randint(-250,250)
+  new_ypos = rand.randint(-75,250)
   bubble.goto(new_xpos, new_ypos)
+
 
 def bubble_clicked(x,y):
   bubble.hideturtle()
   bubble.penup()
   change_position()
+  new_xpos = rand.randint(-250,250)
+  new_ypos = rand.randint(-75,250)
+  bubble.goto(new_xpos, new_ypos)
   bubble.showturtle()
-#-----events----------------
+
+
 bubble.onclick(bubble_clicked)
 
-#bubble.bgpic('halloween-pusheen.gif')
-wn=trtl.Screen ()
-wn.mainloop ()
+wn.mainloop()
